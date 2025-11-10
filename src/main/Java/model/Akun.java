bb93d57079bcd11f460cf65b2985ab39dd454eac
zulfanurlatifah-design <zulfanurlatifah@upi.edu>
2025-11-10 16:10:23 +0700

    Update Akun.java

--- src/main/Java/model/Akun.java
+++ src/main/Java/model/Akun.java
@@ -1,19 +1,54 @@
-public class Admin extends Akun {
-    private int idAdmin;
-    private String namaLengkap;
-    private String alamat;
-    private String noTelepon;
-    private String email;
+// package models; // Sesuaikan nama package Anda
+import java.util.Date;
 
-    public Admin(int idAkun, int idAdmin, String namaLengkap, String alamat,
-                 String noTelepon, String email, String username, String password) {
-        super(idAkun, username, password); // Memanggil konstruktor dari Akun
-        this.idAdmin = idAdmin;
+// Abstrak, karena tidak boleh ada objek "Akun"
+public abstract class Akun {
+    // Atribut (protected agar bisa diakses turunan)
+    protected int idAkun;
+    protected String namaLengkap;
+    protected String alamat;
+    protected String noTelepon;
+    protected String email;
+    protected String username;
+    protected String password; // Nanti harus di-hash!
+
+    // Constructor
+    public Akun(String namaLengkap, String alamat, String noTelepon, String email, String username, String password) {
         this.namaLengkap = namaLengkap;
         this.alamat = alamat;
         this.noTelepon = noTelepon;
         this.email = email;
+        this.username = username;
+        this.password = password;
     }
 
-}
+    // Abstract method (wajib di-override oleh anak)
+    public abstract String getRole();
 
+    // Getter dan Setter (buat untuk semua atribut)
+    public int getIdAkun() {
+        return idAkun;
+    }
+
+    public void setIdAkun(int idAkun) {
+        this.idAkun = idAkun;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public void setNamaLengkap(String namaLengkap) {
+        this.namaLengkap = namaLengkap;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    // ... getter/setter lainnya untuk alamat, noTelepon, email ...
+}
